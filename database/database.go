becb9da5d7ee06d9765bb38be519ce2b7231d593
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2023-12-18 17:36:41 +0900

    db connection/create user table

---
+++ database/database.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type User struct {
+	ID int `db:"id" json:"id"`
+	Name  string `db:"name" json:"name"`
+	Email string `db:"email" json:"email"`
+	Password string `db:"password" json:"password"`
+	CreatedAt string `db:"created_at" json:"created_at"`
+	UpdatedAt string `db:"updated_at" json:"updated_at"`
+}
+
+func main() {
+	// dsn := os.Getenv("DSN")
+	//DBに接続する
+	db, err := sqlx.Open("mysql","root:ajs2b0ti@tcp(localhost:3306)/house_account_book")
+	if err != nil {
+		//現在の関数が終了する時点で、データベースの接続をクローズすること
+		// defer db.Close()
+    log.Fatal(err)
+	}
+
+	//SQL実行
+	rows,err := db.Queryx("SELECT id,name,email,password FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	results := make([]User,0)
+	//データを一行ずつ取得
+	for rows.Next(){
+		var user User
+
+
+//user変数にクエリ結果を格納
+		err := rows.StructScan(&user)
+
+		if err != nil{
+			log.Fatal(err)
+		}
+
+		results = append(results, user)
+	}
+
+	fmt.Println(results)
+
+	// テーブルのマイグレーション
+	// db.AutoMigrate(&User{})
+
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module example.com/m
+
+go 1.21.2
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/mysql v1.5.2 // indirect
+	gorm.io/driver/postgres v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
+)
