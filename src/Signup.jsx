9f500a02b07e00f97ecce4cdec6523599214adc1
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2023-12-30 18:24:41 +0900

    change details

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,4 +1,4 @@
-import React,{useState}from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Login from "./Login";
@@ -11,7 +11,7 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const [errorMessage, setErrorMessage] = useState("");
 
   //フォームが送信されてもページが再読み込みされず、非同期処理が行われる
   const handleSignup = async (e) => {
@@ -30,7 +30,6 @@ function Signup() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "http://localhost:8080/",
         },
         body: JSON.stringify(formData),
       });
@@ -39,8 +38,14 @@ function Signup() {
       if (response.ok) {
         navigate('/Login');
       } else {
-        // エラーメッセージの表示
-        throw new Error("Sign up failed");
+        //APIからのレスポンスをjson形式に解析
+        const data = await response.json();
+        if (data && data.error) {
+          setErrorMessage(data.error);
+        } else {
+          // エラーメッセージの表示
+          throw new Error("Sign up failed");
+        }
       }
     } catch (error) {
       console.error("Error:", error);
@@ -51,8 +56,9 @@ function Signup() {
     <div class="wrapper">
       <div class="container">
         <h1>Welcome</h1>
+        <p></p>
         <form className="form" onSubmit={handleSignup}>
-          <input type="text" placeholder="Name" value={name} onChange={(e)=> setName(e.target.value)} />
+          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
           <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit" id="login-button">Sign up</button>
