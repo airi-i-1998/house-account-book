406bf3ac78071128f4d88a02df14524ce430160c
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2024-01-07 16:25:39 +0900

    create home page

---
+++ src/Home.jsx
@@ -0,0 +1,29 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import jaLocale from '@fullcalendar/core/locales/ja';
+function Home() {
+
+  return (
+    <div class="calendar">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        locales={[jaLocale]}
+        locale='ja'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek listWeek',
+        }}
+        events={[
+          {title:'eventを', start: '2022-03-14'},
+          {title:'こんな感じで追加できます', start: '2022-03-15', end: '2022-03-17'}
+        ]}
+      />
+    </div>
+  );
+}
+
+export default Home;
