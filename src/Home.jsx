3ff2622a858de3054bd54902b25085334c44cfe4
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2024-01-27 11:52:12 +0900

    create sidemenu

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,25 +3,29 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import jaLocale from '@fullcalendar/core/locales/ja';
+import SideMenu from './components/SideMenu';
 function Home() {
 
   return (
-    <div class="calendar">
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
-        initialView="dayGridMonth"
-        locales={[jaLocale]}
-        locale='ja'
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek listWeek',
-        }}
-        events={[
-          {title:'eventを', start: '2022-03-14'},
-          {title:'こんな感じで追加できます', start: '2022-03-15', end: '2022-03-17'}
-        ]}
-      />
+    <div className="calendar-wrap">
+      <SideMenu />
+      <div className="calendar">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+          initialView="dayGridMonth"
+          locales={[jaLocale]}
+          locale='ja'
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek listWeek',
+          }}
+          events={[
+            { title: 'eventを', start: '2022-03-14' },
+            { title: 'こんな感じで追加できます', start: '2022-03-15', end: '2022-03-17' }
+          ]}
+        />
+      </div>
     </div>
   );
 }
